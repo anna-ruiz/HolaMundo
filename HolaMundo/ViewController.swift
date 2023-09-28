d9e0cadd88860ae6b80fb9ae68234a0e3693e2e5
annaruiz <anna.ruiz.blazquez@cieep.com>
2023-09-28 13:11:23 +0200

    prueba

--- HolaMundo/ViewController.swift
+++ HolaMundo/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lbTexto: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func escribeSaludo(_ sender: Any) {
+        lbTexto.text = "Hola Mundo! :)"
+    }
+    
 }
 
